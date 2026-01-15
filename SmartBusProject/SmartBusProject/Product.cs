dcea4936990f83d36e131d0e594c8582b28bea94
dimzet <99910444+dimzet27jr@users.noreply.github.com>
2026-01-15 18:11:57 +0200

    Version 4

---
+++ SmartBusProject/SmartBusProject/Product.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SmartBusProject
+{
+    public class Product
+    {
+        private String name;
+        private double price;
+
+        public Product (string name, double price)
+        {
+            this.name = name;
+            this.price = price;
+        }
+
+        public String getName() {  return name; }
+
+        public double getPrice() { return price; }
+    }
+}
